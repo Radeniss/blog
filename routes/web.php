1d91074d1c0f47428f93398bb7a52a97aaeb3c15
radeniss <mohammad.220170208@mhs.unimal.ac.id>
2024-11-13 10:58:39 +0700

    add eager loading

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function (post $post) {
@@ -28,14 +30,14 @@ Route::get('/posts/{post:slug}', function (post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('category', 'author');
 
-    // $post = Post::find($id);
     return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load('category', 'author');
 
-    // $post = Post::find($id);
     return view('posts', ['title' => ' Arricles in : ' . $category->name, 'posts' => $category->posts]);
 });
 
