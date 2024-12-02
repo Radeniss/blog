070b16105ab6ac5b1b39684019e8619d233e126e
radeniss <mohammad.220170208@mhs.unimal.ac.id>
2024-12-02 12:20:37 +0700

    searching done

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/about', function () {
 
 Route::get('/posts', function () {
     // $posts = Post::with(['author', 'category'])->latest()->get();
-    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category']))->latest()->get()]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
 });
 
 Route::get('/posts/{post:slug}', function (post $post) {
